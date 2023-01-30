cdca29ed756d4b15b69fa524266ae01a9ddd20ba
Rupika17 <rupikasahariya@gmail.com>
2023-01-30 12:54:27 +0530

    setting ssr

--- chill-flix/src/index.js
+++ chill-flix/src/index.js
@@ -8,8 +8,9 @@ import "swiper/css/navigation";
 import "aos";
 import "aos/dist/aos.css";
 import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.hydrate(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -17,3 +18,4 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+reportWebVitals();
